19dc3c4b4ab36d8f5fbc91c76c2ecd6271ce49c4
Ahmed Zaher <ahmedzaher5788@gmail.com>
2024-10-04 09:17:04 +0300

    Refactor encoder docs

--- p2p/encoder.go
+++ p2p/encoder.go
@@ -24,9 +24,10 @@ func (Decoder GOBDecoder) Decode(reader io.Reader, msg *RPC) error {
 // DefaultDecoder is a struct that implements the Decoder interface using a custom decoding method.
 type DefaultDecoder struct{}
 
-// Decode reads from the provided io.Reader and decodes the data into the provided message.
-// It reads up to 1024 bytes into a buffer and assigns it to the message's Payload field.
-// Returns an error if the reading fails.
+// Decode reads data from the provided io.Reader and decodes it into the given RPC message.
+// It first peeks at the first byte to determine if the incoming data is a stream.
+// If it is a stream, it sets the Stream field of the RPC message to true and returns without further reading.
+// Otherwise, it reads up to 1024 bytes from the reader into the Payload field of the RPC message.
 func (Decoder DefaultDecoder) Decode(reader io.Reader, msg *RPC) error {
 	peekBuf := make([]byte, 1)
 	if _, err := reader.Read(peekBuf); err != nil {
